c68c0ce450fdb5a9c54d293270c52493af6ff634
Aleksandar <avelinov2@gmail.com>
2015-12-26 21:48:00 +0100

    commit 25

--- app/src/main/java/com/example/aleksandar/exploregit/MainActivity.java
+++ app/src/main/java/com/example/aleksandar/exploregit/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //bb
         Button b12 = new Button(getBaseContext());
+        //bb
     }
 }
