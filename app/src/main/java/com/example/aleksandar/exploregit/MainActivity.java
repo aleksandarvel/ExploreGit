dfa82a51a4a76c8d0f29f58a8ef84d4ba9a21bdb
Aleksandar <avelinov2@gmail.com>
2015-12-26 21:45:26 +0100

    commit 3

--- app/src/main/java/com/example/aleksandar/exploregit/MainActivity.java
+++ app/src/main/java/com/example/aleksandar/exploregit/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.aleksandar.exploregit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //bb
+        Button b12 = new Button(getBaseContext());
     }
 }
